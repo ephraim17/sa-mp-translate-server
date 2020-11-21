ecac0a7d6aae24c882911410d9af6029e70abaf2
DayWalker[MM] <emulilo17@gmail.com>
2020-11-21 04:18:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -198,5 +198,92 @@ app.post('/lang-to-english', function (req, res) {
 
 })
 
+app.post('/lang-to-spanish', function (req, res) {
+
+    original_msg = req.body.message;
+    player_id = req.body.playerid;
+    res_url = req.body.response_url;
+
+ 
+    const detectLanguage = async (text) => {  
+        try {     
+            let response = await translate.detect(text);    
+            return response[0].language;    
+        } catch (error) {         
+            console.log(`Error at detectLanguage --> ${error}`);               
+            return 0;  
+        }                   
+    }             
+    detectLanguage(original_msg)               
+    .then((language) => {                                          
+        console.log('The request language is => '+ language); 
+
+    if (language != 'es') {
+
+        //Translate the messages to Spanish
+
+        const translateText = async (text, targetLanguage) => {                    
+
+            try {                  
+                let [response] = await translate.translate(text, targetLanguage);      
+
+                return response;             
+ 
+            } catch (error) {                                           
+                console.log(`Error at translateText --> ${error}`);
+                    
+                return 0;                                       
+            }            
+        };
+
+        translateText(original_msg, 'es')
+            
+        //Send the translated message to the server  
+
+        .then((res) => {
+            // console.log('this is your message in Russian ' + res);                                 
+
+            // Convert the Cryllic Russian message to Latin
+                
+            latin_spanish_chat = res;
+
+            let todo = {
+                "detectedLanguageCode": language,
+                "translatedLanguageCode": 'es',
+                "playerID": player_id,
+                "latinTranslatedMessage": latin_spanish_chat
+            };
+            
+            
+            fetch(res_url, {
+                method: 'POST',
+                body: JSON.stringify(todo),
+                headers: { 'Content-Type': 'application/json' }
+            }).then(res => console.log("Original message " + original_msg + "\n" + "Translated Message " + latin_spanish_chat));
+
+            // console.log('This is your message in readable russian ' + latin_russian_chat);
+
+            // Store current user
+
+            // Get current user 
+
+      
+        })
+
+        .catch((err) => {   
+            console.log(err);
+
+
+        });
+
+        //end of send translation 
+    } 
+}) 
+
+    res.send('Success! POST request with translation has been sent to ' + res_url)
+
+
+})
+
 
 app.listen(process.env.PORT , () => console.log('Server started on ' + process.env.PORT ))
